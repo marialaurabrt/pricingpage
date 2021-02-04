459b03f48c3768357ed76af13129763bdc2ac231
Lau Barreto <marialaurabrt@gmail.com>
2021-02-04 01:50:45 -0300

    primeros archivos en el repo

---
+++ scripts.js
@@ -0,0 +1,16 @@
+let slider = document.getElementById("slider");
+let price = document.getElementById("price");
+let chats = document.getElementById("chats");
+
+let checkbox = document.getElementById("checkbox");
+let text = document.getElementById("period");
+
+var prices = [12, 18, 24, 30, 36, 42, 48, 54, 60, 66]; // Array posibles precios (interactivo)
+
+console.log(price)
+
+function calculaDescuento(){
+    if checkbox = true{
+        price.innerHTML = price
+    }
+}
